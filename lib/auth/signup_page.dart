5d821e3db12a44492065eae906dbb3d3b5442457
Marvin Yu <marvinyu02@gmail.com>
2024-07-30 13:47:43 -0400

    add user details from signup page

--- lib/auth/signup_page.dart
+++ lib/auth/signup_page.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:date_field/date_field.dart';
 
 class SignupPage extends StatefulWidget {
   final VoidCallback showLoginPage;
@@ -16,33 +18,59 @@ class _SignupPageState extends State<SignupPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  DateTime userBirthDate = DateTime.now();
+
 
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
     _confirmController.dispose();
+    _firstNameController.dispose();
+    _lastNameController.dispose();
     super.dispose();
   }
 
   Future signUp() async {
-    if (passwordConfirmed()) {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: _emailController.text.trim(), 
-        password: _passwordController.text.trim()
+    try {
+      //create user
+      if (passwordConfirmed()) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(), 
+          password: _passwordController.text.trim()
+        );
+      }
+
+      //add user details
+      addUserDetails(
+        _firstNameController.text.trim(), 
+        _lastNameController.text.trim(), 
+        _emailController.text.trim(),
+        Timestamp.fromDate(userBirthDate),
       );
-    } else {
+
+    } on Exception catch (e) {
       showDialog(
         context: context, 
         builder: (context) {
           return AlertDialog(
-            content: Text('Passwords do not match')
+            content: Text(e.toString())
           );
         }
       );
     }
   }
 
+  Future addUserDetails(String firstName, String lastName, String email, Timestamp birthDate) async {
+    await FirebaseFirestore.instance.collection('users').add({
+      'First Name': firstName,
+      'Last Name': lastName,
+      'Email': email,
+      'Birth Date': birthDate,
+    });
+  }
   bool passwordConfirmed() {
     return _passwordController.text.trim() == _confirmController.text.trim();
   }
@@ -65,6 +93,79 @@ class _SignupPageState extends State<SignupPage> {
                 ),
               ),
             SizedBox(height:40),
+
+            //firstName textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 400),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: TextField(
+                    controller: _firstNameController,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'First Name',
+                    )
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height:20),
+
+            //lastName textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 400),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: TextField(
+                    controller: _lastNameController,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Last Name',
+                    )
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height:20),
+
+            //birth date selector
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 400),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: DateTimeFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Birth Date',
+                    ),
+                    mode: DateTimeFieldPickerMode.date,
+                    lastDate: DateTime.now(),
+                    initialPickerDateTime: DateTime.now(),
+                    onChanged: (DateTime? value) {
+                      userBirthDate = value!;
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height:20),
           
             //email textfield
             Padding(
--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:ttrc_project/auth/main_page.dart';
+import 'package:intl/intl_standalone.dart' if (dart.library.html) 'package:intl/intl_browser.dart';
 
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await findSystemLocale();
 
   runApp(const MyApp());
 }
